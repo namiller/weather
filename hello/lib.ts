7a54b51906ef032f3ffa2c1e94f05dbb2c9e87cd
namiller <namiller@usc.edu>
2019-03-31 23:45:43 -0700

    [clean up]Remove hello world code

--- hello/lib.ts
+++
@@ -1,3 +0,0 @@
-export function generateGreeting(to: string) {
-  return `Hello ${to}.`;
-}
--- hello/main.ts
+++
@@ -1,9 +0,0 @@
-import {generateGreeting} from "weather/hello/lib";
-
-// argv[0] is node, argv[1] is hello, so we want argv[2].
-let name = "World";
-if (process.argv.length >= 3) {
-  name = process.argv[2];
-}
-
-console.log(generateGreeting(name));
